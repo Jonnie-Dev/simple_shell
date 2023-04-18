6d3883fbcd0a83d5dbf8135bfe56b31ff9e659d3
Jonnie-Dev <jtech2096@gmail.com>
2023-04-18 23:14:20 +0100

    feat: added prompt to request user input

--- Eshell/main.c
+++ Eshell/main.c
@@ -9,5 +9,17 @@
 
 int main(int ac, char **argv)
 {
+char *prompt = "(User) ~$";
+char *lineptr;
+size_t n = 0;
+
+/* declaring void variables */
+(void)ac; (void)argv;
+
+printf("%s", prompt);
+getline(&lineptr, &n, stdin);
+printf("%s\n", lineptr);
+
+free(lineptr);
 return (0);
 }
