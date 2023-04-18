9a4e7fa75d15c22013d588a2400370469ff1675d
Jonnie-Dev <jtech2096@gmail.com>
2023-04-18 22:52:01 +0100

    feat: setup boilerplace for C

---
+++ Eshell/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - boiler plate code
+ * @ac: first variable
+ * @argv: second variable
+ * Return: returns 0
+*/
+
+int main(int ac, char **argv)
+{
+return (0);
+}
