0e37f537225ff9802e5bb145f207b23d33da93ee
JonnieDev <jtech2096@gmail.com>
2023-04-25 19:19:22 +0000

    main.c reference commit

--- getinfo.c
+++ getinfo.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * clear_info - it initializes info_t struct
+ * clear_info - initializes info_t struct
  * @info: struct address
  */
 
@@ -14,11 +14,10 @@ void clear_info(info_t *info)
 }
 
 /**
- * set_info - it initializes info_t struct
+ * set_info - initializes info_t struct
  * @info: struct address
  * @av: argument vector
  */
-
 void set_info(info_t *info, char **av)
 {
 	int i = 0;
@@ -47,31 +46,31 @@ void set_info(info_t *info, char **av)
 }
 
 /**
- * free_info - it frees info_t struct fields
+ * free_info - frees info_t struct fields
  * @info: struct address
- * @all: freeing all fields
+ * @all: true if freeing all fields
  */
 
 void free_info(info_t *info, int all)
 {
-	ffree(info->argv);
-	info->argv = NULL;
-	info->path = NULL;
-	if (all)
-	{
-		if (!info->cmd_buf)
-			free(info->arg);
-		if (info->env)
-			free_list(&(info->env));
-		if (info->history)
-			free_list(&(info->history));
-		if (info->alias)
-			free_list(&(info->alias));
-		ffree(info->environ);
-			info->environ = NULL;
-		bfree((void **)info->cmd_buf);
-		if (info->readfd > 2)
-			close(info->readfd);
-		_putchar(BUF_FLUSH);
-	}
+ffree(info->argv);
+info->argv = NULL;
+info->path = NULL;
+if (all)
+{
+if (!info->cmd_buf)
+free(info->arg);
+if (info->env)
+free_list(&(info->env));
+if (info->history)
+free_list(&(info->history));
+if (info->alias)
+free_list(&(info->alias));
+ffree(info->environ);
+info->environ = NULL;
+bfree((void **)info->cmd_buf);
+if (info->readfd > 2)
+close(info->readfd);
+_putchar(BUF_FLUSH);
+}
 }
